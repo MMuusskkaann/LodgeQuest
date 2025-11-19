ca7e3fc8261291be202109cc34a6536dde409c66
Muskan Chaudhary <muskanchaudhry152005@gmail.com>
2025-11-19 23:13:39 +0530

    Basic set up

--- app.js
+++ app.js
@@ -2,36 +2,92 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/LodgeQuest";
 
-main().then(() => {
+main()
+  .then(() => {
     console.log("Connected to DB");
-}).catch(err => {
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+  await mongoose.connect(MONGO_URL);
 }
 
-app.get("/", (req,res) => {
-    res.send("Hii i am root..");
+app.get("/", (req, res) => {
+  res.send("Hii i am root..");
 });
 
-app.get("/testListing",async (req,res) => {
-   let sampleListing = new Listing({
-    title:"My new Villa",
-    description : "By the Beach",
-    price : 1500,
-    location : "Calangute, Goa",
-    country : "India",
-   });
-   await sampleListing.save();
-   console.log("sample was saved");
-   res.send("Successful testing");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+//index route
+app.get("/listings", async (req, res) => {
+  const allListing = await Listing.find({});
+  res.render("listings/index.ejs", { allListing });
+});
+
+//new route
+app.get("/listings/new", async (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+//show route
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
+
+//create route
+app.post("/listings", async (req, res) => {
+  // let {title,description,image,price,country,location} = req.body;/
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+});
+
+//edit route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs",{ listing });
+});
+
+//update route
+app.put("/listings/:id", async (req,res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
 })
 
-app.listen(8080,() => {
-    console.log("Server Listening to port 8080");
+//delete route
+app.delete("/listings/:id", async (req,res) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+})
+// app.get("/testListing",async (req,res) => {
+//    let sampleListing = new Listing({
+//     title:"My new Villa",
+//     description : "By the Beach",
+//     price : 1500,
+//     location : "Calangute, Goa",
+//     country : "India",
+//    });
+//    await sampleListing.save();
+//    console.log("sample was saved");
+//    res.send("Successful testing");
+// })
+
+app.listen(8080, () => {
+  console.log("Server Listening to port 8080");
 });
